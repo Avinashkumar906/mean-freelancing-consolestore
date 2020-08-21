6a1e2a243ff1531a3eb0041d96b7935a8eac55e4
Avinash <Avinash@tracfone.com>
2020-08-21 13:03:24 +0530

    about and contact html pages

--- express/server.js
+++ express/server.js
@@ -11,23 +11,27 @@ if(!process.env.PORT){
 //mongo setup
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true,useUnifiedTopology: true},async (err)=>{
     // for admin user registration code
-    if(!err){
-        let user = await User.findOne({email:process.env.ADMIN_EMAIL})
-        if(!user){
-            user = new User({
-                name : process.env.ADMIN_NAME,
-                email: process.env.ADMIN_EMAIL.toLowerCase(),
-                password:bcrypt.hashSync(process.env.ADMIN_PSWD,10),
-                role:process.env.ADMIN_ROLE
-            })
-            user.save((res)=>console.log(`Admin Registered! check enviroment to view admin email/password`))
-        } else {
-            console.log(`Admin Registered! check enviroment to view admin email/password`)
-        }
-        console.log(`Mongodb Up! <br/>`);
-    } else{
-        console.log(`${err}`);
-    } 
+    try {
+        if(!err){
+            let user = await User.findOne({email:process.env.ADMIN_EMAIL})
+            if(!user){
+                user = new User({
+                    name : process.env.ADMIN_NAME,
+                    email: process.env.ADMIN_EMAIL.toLowerCase(),
+                    password:bcrypt.hashSync(process.env.ADMIN_PSWD,10),
+                    role:process.env.ADMIN_ROLE
+                })
+                user.save((res)=>console.log(`Admin Registered! check enviroment to view admin email/password`))
+            } else {
+                console.log(`Admin exist! check enviroment to view admin email/password`)
+            }
+            console.log(`Mongodb Up! <br/>`);
+        } else{
+            console.log(`${err}`);
+        }    
+    } catch (error) {
+        console.log(`${error}`);
+    }
 });
 
 
